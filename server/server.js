6ea2a268797a807d203fbcc51f99416d421aa9f8
dereck-alex18 <dereck_alexander@outlook.com>
2018-12-04 18:54:46 +0100

    Add a patch route to update todos

--- server/server.js
+++ server/server.js
@@ -75,6 +75,37 @@ app.delete('/todos/:id', (req, res) => {
 
 });
 
+//This route deal with updating a todo
+app.patch('/todos/:id', (req, res) => {
+    const body = {text: req.body.text, completed: req.body.completed};
+    const id = req.params.id;
+
+    if(typeof body.completed === "boolean" && body.completed){
+        body.completedAt = new Date().getTime();
+    }else{
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body}, {$new: true})
+    .then((todo) => {
+        if(!(ObjectID.isValid(req.params.id))){
+            return res.status(404).send();
+        }
+
+        if(!todo){
+            res.status(404).send();
+        }
+
+        res.send({body});    
+    })
+    .catch((e) => {
+        res.status(400).send();
+    });
+    
+    
+});
+
 app.listen(port, () => {
     console.log(`Server started at port: ${port}`);
 });
