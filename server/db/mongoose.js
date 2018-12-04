260c72eddd03a2eca953eccb67be494c469c4f58
dereck-alex18 <dereck_alexander@outlook.com>
2018-12-04 15:28:00 +0100

    Setup environment variables on heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-//mongoose.connect('mongodb://localhost:27017/TodoApp');
-mongoose.connect('mongodb://alexport:todosAPI18@ds119663.mlab.com:19663/todos');
+mongoose.connect(process.env.DATABASEURL);
+
 
 module.exports = {
     mongoose
