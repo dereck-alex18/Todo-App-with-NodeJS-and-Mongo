8d32afa7ab22a7e900f3c0052676217dad742540
dereck-alex18 <dereck_alexander@outlook.com>
2018-12-04 14:43:53 +0100

    Setup mLAB URI

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 //mongoose.connect('mongodb://localhost:27017/TodoApp');
-mongoose.connect('mongodb://alexport:todoAPI18@ds119663.mlab.com:19663/todos');
+mongoose.connect('mongodb://alexport:todosAPI18@ds119663.mlab.com:19663/todos');
 
 module.exports = {
     mongoose
