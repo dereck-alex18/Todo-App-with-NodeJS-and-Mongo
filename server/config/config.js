bd816b68ca08418047466daec7dc2aac7567422a
dereck-alex18 <dereck_alexander@outlook.com>
2018-12-14 18:12:16 +0100

    Changed the config file to not have issues on heroku

--- server/config/config.js
+++ server/config/config.js
@@ -1,9 +1,10 @@
 //This file will set all the environment variables the db url, the port as well as tje JWT secret
 
 const env = process.env.NODE_ENV || 'development';
-const config = require('./config.json');
+
 
 if(env === 'development' || env === 'test'){
+    const config = require('./config.json');
     //Setting the environment variables
     let configEnv =  config[env]; //configEnv depends on the env value
     //Object.keys converts the keys of an object into an array
